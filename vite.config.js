587c4bb24532d58f9e7864601cccfe641b47be4e
Clement Denis <le.mikmac@gmail.com>
2025-06-23 16:36:52 +0100

    add the web stuff

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { join } from 'node:path'
+
+import tailwindcss from '@tailwindcss/vite'
+import preact from '@preact/preset-vite'
+import { defineConfig } from 'vite'
+import Sonda from 'sonda/vite'
+
+
+export default defineConfig({
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      // https://rollupjs.org/configuration-options/
+    },
+  },
+  root: join(import.meta.dirname, './web'),
+  plugins: [
+    preact({ jsxImportSource: 'preact' }),
+    tailwindcss(),
+    Sonda({
+      enabled: true,
+      format: 'html',
+    }),
+  ],
+})
